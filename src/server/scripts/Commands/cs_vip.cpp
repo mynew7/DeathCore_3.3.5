e82516f134a424283c3aafa0404b2a27ac64ac81
Noffearr <noffearrdeathproject@gmail.com>
2015-02-13 02:54:50 -0300

    DeathCore_3.3.5a
    Atualizado, Modificado e Fixado os comandos para V.I.Ps!
    Comandos para V.I.Ps Disponíveis:
    .Vip Buff (Adiciona automaticamente alguns Buffs Especiais)
    .Vip Sala (Teletransporte para a Área V.I.P)
    .Vip Raça ( Usado para mudar sua Raça)
    .Vip Facção (Usado para mudar sua Facção)
    .Vip Skills (Deixa suas Skills no nível máximo)
    .Vip Tele (Usado para Teletransportar-se)
    .Vip Forma (Usado para mudar o Display do Jogador)
    
    Em breve mais atualizações!

--- src/server/scripts/Commands/cs_vip.cpp
+++ src/server/scripts/Commands/cs_vip.cpp
@@ -21,9 +21,8 @@
 #include "Chat.h"
 #include "Common.h"
 #include "Language.h"
-
-
-uint32 aurasVip[] = {15366, 16609, 48162, 48074, 48170, 43223, 36880, 467, 69994, 33081, 24705, 26035, 48469}; // Buff Vips
+#include "CellImpl.h"
+#include "GridNotifiers.h"
 
 class vipcommands : public CommandScript
 {
@@ -34,14 +33,16 @@ public:
     {
         static ChatCommand vipCommandTable[] =
 
-        { 
-	    {"Buff", SEC_VIP, true, &HandleVipBuffCommand, "", NULL},
-		{ "Sala-Vip",	    SEC_VIP,     true, &HandleVipMallCommand,         "", NULL },
-        { "Mudar-Raça",    SEC_VIP,  false, &HandleChangeRaceCommand,             "", NULL },
-	    { "Mudar-Facção",	SEC_VIP,  false, &HandleChangeFactionCommand,		"", NULL },
-	    { "Mudar-Visual",	SEC_VIP,  false, &HandleCustomizeCommand,		"", NULL },
- 
-            { NULL,             0,                     false, NULL,                                           "", NULL }
+        {
+		{ "Buff",           SEC_VIP,  false, &HandleBuffCommand,		"", NULL },
+		{ "Sala",	    SEC_VIP,     true, &HandleVipMallCommand,         "", NULL },
+        { "Raça",    SEC_VIP,  false, &HandleChangeRaceCommand,             "", NULL },
+	    { "Facção",	SEC_VIP,  false, &HandleChangeFactionCommand,		"", NULL },
+	    { "Skills",	SEC_VIP,  false, &HandleMaxSkillsCommand,		"", NULL },
+	    { "Visual",	SEC_VIP,  false, &HandleCustomizeCommand,		"", NULL },
+	    { "Tele",           SEC_VIP,  false, &HandleTeleCommand,		"", NULL },
+	    { "Forma",           SEC_VIP,  false, &HandleMorphCommand,		"", NULL },
+        { NULL,             0,                     false, NULL,                                          "", NULL }
         };
         static ChatCommand commandTable[] =
         {
@@ -51,24 +52,110 @@ public:
         return commandTable;
     }
 
+static bool HandleActivateCommand(ChatHandler * handler, const char * args)
+{
+	Player* player = handler->GetSession()->GetPlayer();
+
+	if(player->GetSession()->GetSecurity() >= 1)
+	{
+		handler->PSendSysMessage("Você já é um Jogador VIP");
+		handler->SetSentErrorMessage(true);
+		return false;
+	}
+
+	if(player->HasItemCount(313370, 1, false)) // Token ID, Count.
+	{
+                  PreparedStatement* stmt = LoginDatabase.GetPreparedStatement(LOGIN_INS_ACCOUNT_ACCESS);
+                  stmt->setUInt32(0, player->GetSession()->GetAccountId());
+                  stmt->setUInt8(1, 1);
+                  stmt->setInt32(2, 1);
+	         LoginDatabase.Execute(stmt);
+		player->DestroyItemCount(313370, 1, true, false); // Token ID, Count.
+		handler->PSendSysMessage("Você agora é um Jogador VIP, para ativa-lo relogue e entre no jogo novamente");
+		return true;
+	}
+	return true;
+}
+
+static bool HandleTeleCommand(ChatHandler* handler, const char* args)
+    {
+        if (!*args)
+            return false;
+
+        Player* me = handler->GetSession()->GetPlayer();
+
+        // id, or string, or [name] Shift-click form |color|Htele:id|h[name]|h|r
+        GameTele const* tele = handler->extractGameTeleFromLink((char*)args);
+
+        if (!tele)
+        {
+            handler->SendSysMessage(LANG_COMMAND_TELE_NOTFOUND);
+            handler->SetSentErrorMessage(true);
+            return false;
+        }
+
+        if (me->IsInCombat())
+        {
+            handler->SendSysMessage(LANG_YOU_IN_COMBAT);
+            handler->SetSentErrorMessage(true);
+            return false;
+        }
+
+        MapEntry const* map = sMapStore.LookupEntry(tele->mapId);
+        if (!map || map->IsBattlegroundOrArena())
+        {
+            handler->SendSysMessage(LANG_CANNOT_TELE_TO_BG);
+            handler->SetSentErrorMessage(true);
+            return false;
+        }
+
+        // stop flight if need
+        if (me->IsInFlight())
+        {
+            me->GetMotionMaster()->MovementExpired();
+            me->CleanupAfterTaxiFlight();
+        }
+        // save only in non-flight case
+        else
+            me->SaveRecallPosition();
+
+        me->TeleportTo(tele->mapId, tele->position_x, tele->position_y, tele->position_z, tele->orientation);
+        return true;
+		}
 
 
-		static bool HandleVipBuffCommand(ChatHandler * handler, const char * args)
+static bool HandleBuffCommand(ChatHandler * handler, const char * args)
     {
         Player * pl = handler->GetSession()->GetPlayer();
-                if(pl->InArena())
-                {
-                        pl->GetSession()->SendNotification("Você não pode usar este comando em arena!");
-                        return false;
-                }
-       
-                pl->RemoveAurasByType(SPELL_AURA_MOUNTED);
-                for(int i = 0; i < 40; i++)
-                    pl->AddAura(aurasVip[i], pl);
-                handler->PSendSysMessage("|cffFFFFFF[|cffFFA500Buff Vip|cffFFFFFF] |cffFF0000Você está quase imortal!");
-                return true;
- 
+		if(pl->IsInCombat())
+		{
+			pl->GetSession()->SendNotification("Você não pode usar isso em combate!!");
+			return false;
+		}
+		if(pl->InArena())
+		{
+			pl->GetSession()->SendNotification("Você não pode usar isso em uma arena!");
+			return false;
+		}
+		else
+		pl->AddAura(15366, pl);
+		pl->AddAura(16609, pl);
+		pl->AddAura(48162, pl);
+		pl->AddAura(48074, pl);
+		pl->AddAura(48170, pl);
+		pl->AddAura(43223, pl);
+		pl->AddAura(36880, pl);
+		pl->AddAura(467, pl);
+		pl->AddAura(69994, pl);
+		pl->AddAura(33081, pl);
+		pl->AddAura(24705, pl);
+		pl->AddAura(26035, pl);
+		pl->AddAura(48469, pl);
+		handler->PSendSysMessage("|cffFFFFFF[|cffFFA500Buff Vip|cffFFFFFF] |cffFF0000Você está quase imortal!");
+		return true;
+
     }
+
 
 static bool HandlevipCommand(ChatHandler* handler, const char* args)
     {
@@ -79,12 +166,18 @@ static bool HandlevipCommand(ChatHandler* handler, const char* args)
             return true;
     }
 
+static bool HandleMorphCommand(ChatHandler* handler, const char* args)
+    {
+        handler->GetSession()->GetPlayer()->SetDisplayId((uint32)atol((char*)args));
+        return true;
+    }
+
 static bool HandleChangeRaceCommand(ChatHandler* handler, const char* args)
     {
 
         Player* me = handler->GetSession()->GetPlayer();
 		me->SetAtLoginFlag(AT_LOGIN_CHANGE_RACE);
-		handler->PSendSysMessage("Relogue para mudar sua raça.");
+		handler->PSendSysMessage("Relogue para mudar sua raça!");
         return true;
     }
 
@@ -93,21 +186,29 @@ static bool HandleChangeFactionCommand(ChatHandler* handler, const char* args)
 
         Player* me = handler->GetSession()->GetPlayer();
 		me->SetAtLoginFlag(AT_LOGIN_CHANGE_FACTION);
-		handler->PSendSysMessage("Relogue para mudar sua facção.");
+		handler->PSendSysMessage("Relogue para mudar sua facção!");
         return true;
     }
 
+static bool HandleMaxSkillsCommand(ChatHandler* handler, const char* args)
+    {
+
+        Player* me = handler->GetSession()->GetPlayer();
+		me->UpdateSkillsForLevel();
+		handler->PSendSysMessage("Suas habilidades de armas agora estão no nível máximo!");
+        return true;
+    }
 
 static bool HandleCustomizeCommand(ChatHandler* handler, const char* args)
     {
 
         Player* me = handler->GetSession()->GetPlayer();
 		me->SetAtLoginFlag(AT_LOGIN_CUSTOMIZE);
-		handler->PSendSysMessage("Relogue para mudar o seu visual.");
+		handler->PSendSysMessage("Relogue para mudar o seu visual!");
         return true;
     }
-	
-	static bool HandleVipMallCommand(ChatHandler* handler, const char* args)
+
+		static bool HandleVipMallCommand(ChatHandler* handler, const char* args)
     {
 
         Player* me = handler->GetSession()->GetPlayer();
